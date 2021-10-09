ce1d679814f03875eabb1d183f30e4479ae11aa9
waliradim <waliradim@gmail.com>
2021-10-09 20:07:09 +0600

    useRef as Stroge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Form from "./component/UseRef/Form";
+import Time from "./component/UseRef/Time";
 
 function App() {
   return (
     <div>
       <h1>Use React Hook useRef</h1>
+      <Time />
+      <br />
       <Form />
     </div>
   );
