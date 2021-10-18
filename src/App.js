b8df80769b549b31a561d13031d2844fc022c9e1
waliradim <waliradim@gmail.com>
2021-10-18 23:00:49 +0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from "react";
-import LayoutComponentOne from "./component/CustomHook/LayoutComponentOne";
-import LayoutComponentTwo from "./component/CustomHook/LayoutComponentTwo";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "./component/Router/Home";
+import About from "./component/Router/About";
+import Dashbord from "./component/Router/Dashbord";
+import Error from "./component/Router/Error";
+import Post from "./component/Router/Post";
+import Service from "./component/Router/Service";
+import NavBar from "./component/Router/Navbar";
 
 function App() {
+  const isLogIn = true;
+
   return (
-    <div>
-      <h1>Use React Custom Hook </h1>
-      <LayoutComponentTwo />
-    </div>
+    <Router>
+      <NavBar />
+      <h1>Use React Router </h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/Dashbord" component={Dashbord} />
+        {/* <Route
+          exact
+          path="/Service"
+          render={() => <Service work="we are doing something" />}
+        /> */}
+        //another rule for sent props from route
+        <Route exact path="/Service">
+          <Service work="we are doing something" />
+        </Route>
+        <Route exact path="/Post/:category/:topic" component={Post} />
+        <Route exact path="/login">
+          {isLogIn ? <Redirect to="/Dashbord" /> : <Home />}
+        </Route>
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
